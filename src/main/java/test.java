5b4b61ead0181e9958b3b97208dd7897e9de9547
Vishnu <vishnu.kotichintala@macys.com>
2019-06-19 10:48:27 -0400

    added

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,9 +1,5 @@
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
 public class test {
 	public static void main(String[] args) {
-		SpringApplication.run(test.class, args);
+		System.out.println("inside the test application");
 	}
 }
