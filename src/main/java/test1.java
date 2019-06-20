d66e23e71afbb3e5453b41939667d6943be109e6
Vishnu <vishnu.kotichintala@macys.com>
2019-06-20 10:31:19 -0400

    added

--- src/main/java/test1.java
+++ src/main/java/test1.java
@@ -2,8 +2,7 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.web.bind.annotation.*;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+
 
 
 public class test1 {
